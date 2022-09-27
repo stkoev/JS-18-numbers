85d31d9667d620254dbe66388f29de57fa9f74e7
Stefan Koev <st.koev@outlook.com>
2022-09-27 17:56:23 +0300

    implement javascript, final commit

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('numbers project');
+const items = [...document.querySelectorAll(".number")];
+
+const updateCount = function (el) {
+  const value = parseInt(el.dataset.value);
+  console.log(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+
+    if (initialValue > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+
+    el.textContent = `${initialValue}`;
+  }, 10);
+};
+
+items.forEach((item) => {
+  console.log(item);
+  updateCount(item);
+});
